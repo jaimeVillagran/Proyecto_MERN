f69bbd5238aa5e6e2c90b97cb65f8c9978de8753
TJaime <jaime.villagran@gmail.com>
2022-11-29 00:10:37 -0300

    Modificacion App.js y modificacion acceso import

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { onHashChangeHandler } from "../assets/js/route.js";
+import {
+	continent,
+	searchCountry,
+	searchBtn,
+	headerTitle,
+} from "../assets/js/bring.js";
+
+const onLoadFunction = () => {
+	location.hash = "_home";
+};
+
+const onFilterFunction = (continent) => {
+	location.hash = `_filter_${continent}`;
+};
+
+const onSearchFunction = (value) => {
+	if (value.length == 0) {
+		location.hash = "_home";
+		return;
+	}
+	const newValue = value.toLowerCase();
+	const withoutSpaces = newValue.split(" ").join("_");
+	location.hash = `_search_${withoutSpaces}`;
+};
+
+window.addEventListener("load", onLoadFunction);
+
+window.addEventListener("hashchange", () => onHashChangeHandler(location.hash));
+
+continent.forEach((btn) => {
+	btn.addEventListener("click", (e) => onFilterFunction(e.target.textContent));
+});
+
+searchBtn.addEventListener("click", () =>
+	onSearchFunction(searchCountry.value)
+);
+
+headerTitle.addEventListener("click", onLoadFunction);
